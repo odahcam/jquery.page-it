f815592f99677129f921c554f90eb9fbfaa8d8bf
Luiz Filipe Machado Barni <67390@havan.corp>
2017-08-29 11:06:19 -0300

    code rewrite
    removed update meta, using JSON as default dataType, simplified all process, renamed events, improved event registering and triggering, improved logging, ...

--- jquery.page-it.js
+++ jquery.page-it.js
@@ -1,42 +1,128 @@
-(function($, window, document, undefined) {
+(function ($, window, document, undefined) {
 
     var pluginName = 'pageIt';
 
+    var logger = {
+        log: function () {
+            console.log(pluginName + ": " + arguments[0], Array.prototype.slice.call(arguments, 1));
+        },
+        info: function () {
+            console.info(pluginName + ": " + arguments[0], Array.prototype.slice.call(arguments, 1));
+        },
+        warn: function () {
+            console.warn(pluginName + ": " + arguments[0], Array.prototype.slice.call(arguments, 1));
+        },
+        error: function () {
+            console.error(pluginName + ": " + arguments[0], Array.prototype.slice.call(arguments, 1));
+        },
+    };
+
     if (!$) {
-        console.error(pluginName + ': Não foi possível reconhecer o jQuery, inicialização cancelada!');
+        logger.error(pluginName + ': Não foi possível reconhecer o jQuery, inicialização cancelada!');
         return false;
     }
 
+    /*
+     * Polyfill
+     */
+    // Production steps of ECMA-262, Edition 5, 15.4.4.17
+    // Reference: http://es5.github.io/#x15.4.4.17
+    if (!Array.prototype.some) {
+        Array.prototype.some = function (fun/*, thisArg*/) {
+            'use strict';
+
+            if (this == null) {
+                throw new TypeError('Array.prototype.some called on null or undefined');
+            }
+
+            if (typeof fun !== 'function') {
+                throw new TypeError();
+            }
+
+            var t = Object(this);
+            var len = t.length >>> 0;
+
+            var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+            for (var i = 0; i < len; i++) {
+                if (i in t && fun.call(thisArg, t[i], i, t)) {
+                    return true;
+                }
+            }
+
+            return false;
+        };
+    }
+
+
+    var metaSchema = {
+        size: null,
+        first: 1,
+        prev: null,
+        current: null,
+        next: null,
+        last: null,
+        total: null
+    };
+
     var defaults = {
+        /**
+         * @var {int?} The page where the plugin should start.
+         */
+        initPage: null,
+        /**
+         * @var {bool} cache : if should store loaded pages (and load'em from) in a local storage or not
+         */
+        cache: true,
         url: '',
         method: 'get',
-        // dataType: 'html',
-        initPage: 1,
-        cacheAjax: false,
-        cachePagination: true,
+        dataType: 'json',
+        /**
+         * @var {object} jQuery.AJAX configuration options.
+         */
+        ajax: {
+            cache: false,
+            global: true,
+        },
+        /**
+         * @var {HTMLElement} contentView : if you define this, you will have auto page content updates
+         */
         contentView: null,
-        urlMeta: '',
-        pageMeta: {
-            first: 1,
-            prev: undefined,
-            current: undefined,
-            next: undefined,
-            last: undefined,
-            total: undefined
-        }
+        /**
+         * @var {object}
+         */
+        meta: metaSchema
     };
 
     // Constructor
     window[pluginName] = function PageIt(options) {
 
         this.settings = $.extend(true, {}, defaults, options);
-        this.events = {};
+        this.events = {
+            'ready': [],
+            'page.load.empty': [],
+            'page.load.loaded': [],
+            'page.load.autoupdated': [],
+            'page.load.skipped': [],
+            'page.load.first': [],
+            'page.load.last': [],
+            'page.load.error': [],
+            'page.load.before': [],
+            'page.load.after': [],
+            'page.load.cache': [],
+            'page.filled': [],
+            'page.first': [],
+            'page.prev': [],
+            'page.next': [],
+            'page.last': [],
+        };
         this.pages = [];
 
-        this.pageMeta = this.settings.pageMeta;
-        this.pageMeta.prev = this.settings.initPage - 1;
-        this.pageMeta.current = this.settings.initPage;
-        this.pageMeta.next = this.settings.initPage + 1;
+        this.requesting = false;
+
+        this.meta = this.settings.meta;
+        this.meta.prev = this.settings.initPage - 1;
+        this.meta.current = this.settings.initPage;
+        this.meta.next = this.settings.initPage + 1;
 
         this.requestData = {};
 
@@ -49,28 +135,36 @@
         /**
          * Initialize module functionality.
          **/
-        init: function() {
+        init: function () {
+
+            this.trigger('ready');
 
             if (!!this.settings.initPage)
                 this.to(this.settings.initPage);
-
-            this.trigger(pluginName + '.ready');
         },
 
         /**
          * @param {intger} pageIndex
          **/
-        to: function(pageIndex) {
+        to: function (pageIndex) {
 
-            if (!pageIndex || pageIndex > this.pageMeta.last)
-                return this;
+            if (this.requesting === true) {
+                logger.warn('Uma requisição de página já está em andamento, esta requisição será ignorada.');
+                return false;
+            }
+
+            if (!pageIndex || (this.meta.last && pageIndex > this.meta.last)) {
+                this.trigger('page.load.skipped', {});
+                this.trigger('page.load.last', {});
+                return false;
+            }
 
             // if index is a string like 'next' or 'prev', it will be translated by calling it's manager
             if (typeof pageIndex === 'string' && pageIndex === 'next' || pageIndex === 'prev') {
                 return this[pageIndex]();
             }
 
-            if (!this.settings.cachePagination || !this.pages[pageIndex]) {
+            if (!this.settings.cache || !this.pages[pageIndex]) {
 
                 /*
                  * reset requestData object before triggering the beforeLoad,
@@ -84,48 +178,66 @@
 
                 var that = this;
 
+                this.requesting = true;
+
                 $.ajax({
-                    cache: this.settings.cache,
+                    cache: this.settings.ajax.cache,
+                    global: this.settings.ajax.global,
                     url: this.settings.url,
                     method: this.settings.method,
                     data: this.requestData,
-                    // dataType: this.settings.dataType,
-                    success: function(data) {
+                    dataType: this.settings.dataType,
+                    success: function (data, status, response) {
 
-                        that.pages[pageIndex] = data;
+                        /*
+                        data: {
+                            meta: {...}
+                            content: [HTML String]
+                        }
+                        */
 
-                        that.pageMeta.prev = pageIndex - 1;
-                        that.pageMeta.current = pageIndex;
-                        that.pageMeta.next = pageIndex + 1;
+                        that.pages[pageIndex] = data.content;
 
-                        that.trigger('page.content.loaded', data);
+                        that.meta.prev = pageIndex - 1;
+                        that.meta.current = pageIndex;
+                        that.meta.next = pageIndex + 1;
 
-                        if (!!data) {
+                        if (data.meta) {
+                            that.setMeta(data.meta);
+                        }
+
+                        if (!!data.content) {
+
+                            that.trigger('page.load.loaded', data);
 
                             // se tem um container de conteúdo definido e a resposta é em HTML, insere o conteúdo nele
-                            if (/*that.settings.dataType === 'html' && */!!that.settings.contentView) {
+                            if (!!that.settings.contentView) {
 
-                                if ($(that.settings.contentView).html(data)) {
+                                if ($(that.settings.contentView).html(data.content)) {
                                     // plugin .trigger method
-                                    that.trigger('page.filled', data);
+                                    that.trigger('page.load.autoupdated', data);
                                 }
 
                             } else {
-                                console.warn('No container set, no data will be auto inserted.');
+                                logger.warn('No container set, no data will be auto inserted.');
                             }
 
                         } else {
 
-                            that.trigger('page.content.empty', data);
+                            that.trigger('page.load.empty', data);
 
                         }
 
                     },
-                    error: function(response) {
-                        console.error('Erro ao carregar página.');
+                    error: function (response) {
+                        logger.error('Erro ao carregar página.');
                         console.log(response);
+
+                        that.trigger('page.load.error', response);
                     },
-                    complete: function(response) {
+                    complete: function (response) {
+
+                        that.requesting = false;
 
                         // plugin .trigger method
                         that.trigger('page.load.after', response);
@@ -145,12 +257,12 @@
                         this.trigger('page.filled', this.pages[pageIndex]);
                     }
 
-                    this.trigger('page.content.loaded', this.pages[pageIndex]);
-                    this.trigger('page.loaded.cache', this.pages[pageIndex]);
+                    this.trigger('page.load.loaded', this.pages[pageIndex]);
+                    this.trigger('page.load.cache', this.pages[pageIndex]);
 
                 } else {
 
-                    this.trigger('page.content.empty', this.pages[pageIndex]);
+                    this.trigger('page.load.empty', this.pages[pageIndex]);
 
                 }
 
@@ -160,35 +272,35 @@
         },
 
         /**
-         * Calls .to() with page number pageMeta.first as parameter.
+         * Calls .to() with page number meta.first as parameter.
          **/
-        first: function() {
-            this.trigger('page.first', this.pageMeta.first);
-            return this.to(this.pageMeta.first);
+        first: function () {
+            this.trigger('page.first', this.meta.first);
+            return this.to(this.meta.first);
         },
 
         /**
-         * Calls .to() with page number pageMeta.current - 1 as parameter.
+         * Calls .to() with page number meta.current - 1 as parameter.
          **/
-        prev: function() {
-            this.trigger('page.prev', this.pageMeta.next);
-            return this.to(this.pageMeta.prev);
+        prev: function () {
+            this.trigger('page.prev', this.meta.next);
+            return this.to(this.meta.prev);
         },
 
         /**
-         * Calls .to() with page number pageMeta.current + 1 as parameter.
+         * Calls .to() with page number meta.current + 1 as parameter.
          **/
-        next: function() {
-            this.trigger('page.next', this.pageMeta.next);
-            return this.to(this.pageMeta.next);
+        next: function () {
+            this.trigger('page.next', this.meta.next);
+            return this.to(this.meta.next);
         },
 
         /**
-         * Calls .to() with page number pageMeta.last as parameter.
+         * Calls .to() with page number meta.last as parameter.
          **/
-        last: function() {
-            this.trigger('page.last', this.pageMeta.last);
-            return this.to(this.pageMeta.last);
+        last: function () {
+            this.trigger('page.last', this.meta.last);
+            return this.to(this.meta.last);
         },
 
         /**
@@ -197,12 +309,29 @@
          * @param {function} fn
          * @return {object}
          **/
-        on: function(eventName, fn) {
+        on: function (eventName, fn) {
 
-            if (this.events[eventName]) {
-                this.events[eventName].push(fn);
+            if (eventName.match(' ')) {
+                eventname.split(' ').forEach(function (eventName) {
+                    this.on(eventName, fn);
+                });
             } else {
-                this.events[eventName] = [fn];
+                //if (fn.name) {
+                    if (!this.events[eventName]) {
+                        logger.warn('Evento indisponível.');
+                        throw new Error('Can\'t attach unrecognized event handler.');
+                    }
+
+                    var x = this.events[eventName].some(function (t) {
+                        return t === fn;
+                    }) || this.events[eventName].indexOf(fn);
+
+                    //if (x > -1) return this;
+
+                    this.events[eventName].push(fn);
+                //} else {
+                //    throw new Error('Funções anônimas não são permitidas.');
+                //}
             }
 
             return this;
@@ -214,14 +343,14 @@
          * @param {undefined} arguments
          * @example this.trigger('event'[, data, response, etc]);
          **/
-        trigger: function(eventName) {
+        trigger: function (eventName) {
 
             if (this.events[eventName] && this.events[eventName].length) {
 
                 var that = this,
                     args = arguments;
 
-                this.events[eventName].map(function(fnName) {
+                this.events[eventName].map(function (fnName) {
 
                     fnName.apply(that, Array.prototype.slice.call(args, 1)); // Array.prototype.slice will convert the arguments object
 
@@ -232,53 +361,14 @@
             return this;
         },
 
-        /**
-         * Method to get some updated details about pagination.
-         **/
-        updateMeta: function() {
-
-            var that = this;
-
-            $.ajax({
-                cache: false,
-                url: this.settings.urlMeta,
-                method: this.settings.method,
-                data: this.pageMeta,
-                // dataType: 'json',
-                success: function(data) {
-
-                    that.trigger('page.meta.loaded', data);
-
-                    if (data) {
-
-                        $.extend(true, that.pageMeta, data);
-
-                        that.trigger('page.meta.updated', data);
-
-                    } else {
-
-                        that.trigger('page.meta.empty', data);
-                        console.warn('Page meta update found no data.');
-
-                    }
-
-                },
-                error: function(response) {
-                    console.error('Erro ao carregar meta dados.');
-                    console.log(response);
-                },
-                complete: function(response) {
-                    console.groupEnd();
-                }
-            });
-
-            return this;
-        },
-
-        setData: function(object) {
+        setData: function (object) {
 
             $.extend(this.requestData, object);
 
+        },
+
+        setMeta: function (meta) {
+            $.extend(this.meta, meta);
         }
 
     });
